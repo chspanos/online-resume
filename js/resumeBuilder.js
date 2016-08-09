1f6f539ee8d497977848887b639641b076984a51
Cheryl Spanos <chspanos@comcast.net>
2016-08-09 12:35:59 -0700

    Add project images and CSS styling

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -10,6 +10,11 @@ var bio = {
     "github": "chspanos",
     "location": "San Francisco Bay Area"
   },
+  "footercontacts": {
+    "github": "https://github.com/chspanos",
+    "linkedin": "https://www.linkedin.com/in/cheryl-spanos-4b9471a5",
+    "wordpress": "https://writersotj.wordpress.com/"
+  },
   "skills" : [
     "More than five years of software engineering experience in CAD/EDA",
     "Excellent written communication skills",
@@ -70,16 +75,16 @@ var projects = {
     {
       "title": "Portfolio",
       "dates": "2016",
-      "description": "A responsive website showcasing my Udacity Front-End Web \
+      "description": "A responsive website that will be used to showcase my Udacity Front-End Web \
       Developer projects",
-      "images": ["images/197x148.gif", "images/197x148.gif"]
+      "images": ["images/portfolio-img1.jpg", "images/portfolio-img2.jpg"]
     },
     {
       "title": "Online Resume",
       "dates": "2016",
       "description": "An online resume that uses JSON objects to store resume \
-      data and employs javaScript and jQuery to populate the HTML with this data.",
-      "images": ["images/197x148.gif"]
+      data and employs javaScript and jQuery to populate the HTML with this data",
+      "images": ["images/resume-img1.jpg"]
     }
   ]
 };
@@ -134,17 +139,50 @@ var education = {
   ]
 };
 
-// function to display contact info
-// takes as input the selector to which these contacts should be appended
-function displayContacts(selector) {
+// function to display top contact info
+function displayContacts() {
+  // modify the generic contact template to use zocial icons
+  var HTMLmyGeneric = '<li class="flex-item"><span class="orange-text %zocial%"></span><span class="white-text">%data%</span></li>';
+
   // iterate through each contacts object
   for (var contactKey in bio.contacts) {
     if (bio.contacts.hasOwnProperty(contactKey)) {
+      // set the zocial class
+      var zocialClass;
+
+      switch (contactKey) {
+        case "mobile":
+          zocialClass = "zocial-call";
+          break;
+        case "email":
+          zocialClass = "zocial-email";
+          break;
+        case "twitter":
+          zocialClass = "zocial-twitter";
+          break;
+        case "github":
+          zocialClass = "zocial-github";
+          break;
+        case "location":
+          zocialClass = "zocial-guest";
+          break;
+        default:
+          zocialClass = "";
+      };
+
       // format and display each contact
-      var formattedContact = HTMLcontactGeneric.replace("%contact%", contactKey).replace("%data%", bio.contacts[contactKey]);
-      $(selector).append(formattedContact);
+      var formattedContact = HTMLmyGeneric.replace("%zocial%", zocialClass).replace("%data%", bio.contacts[contactKey]);
+      $("#topContacts").append(formattedContact);
     }
   };
+}
+
+// function to add new class to skill items
+function addSkillClass() {
+  var skillsList;
+
+  skillsList = $("#skills").find("li");
+  skillsList.addClass("skill");
 }
 
 // Bio display method definition
@@ -157,7 +195,7 @@ bio.display = function() {
   $("#header").prepend(formattedName);
 
   // format and display contacts
-  displayContacts("#topContacts");
+  displayContacts();
 
   // format and display biopic
   var formattedPic = HTMLbioPic.replace("%data%", bio.biopic);
@@ -178,6 +216,9 @@ bio.display = function() {
       $("#skills").append(formattedSkill);
     });
   }
+
+  // Add skill class to skill list items for styling
+  addSkillClass();
 }
 
 // Work display method definition
@@ -290,6 +331,8 @@ function displaySchools() {
 // Function to display on-line courses
 function displayOnlineCourses() {
   if (education.onlineCourses.length > 0) {
+    // define custom format for URL message with distinct class for styling
+    var HTMLmyOnlineURL = '<br><a class="url" href="#">%data%</a>';
 
     // display heading
     $("#education").append(HTMLonlineClasses);
@@ -313,7 +356,7 @@ function displayOnlineCourses() {
       }
 
       // format and display url link
-      var formattedUrl = HTMLonlineURL.replace("#", thisCourse.url).replace("%data%", thisCourse.url);
+      var formattedUrl = HTMLmyOnlineURL.replace("#", thisCourse.url).replace("%data%", thisCourse.url);
       $(".education-entry:last").append(formattedUrl);
     });
   }
@@ -323,6 +366,48 @@ function displayOnlineCourses() {
 education.display = function() {
   displaySchools();
   displayOnlineCourses();
+}
+
+// function to display footer contact info using zocial icons
+function displayFooter() {
+  // Generic form for footer
+  var HTMLfooterGeneric = '<li class="flex-item"><a class="orange-text %zocial%" href="#"><span class="white-text">%data%</span></a></li>';
+
+  // iterate through each contacts object
+  for (var contactKey in bio.footercontacts) {
+    if (bio.footercontacts.hasOwnProperty(contactKey)) {
+      // set variables
+      var zocialClass, myRef, myData;
+
+      switch (contactKey) {
+        case "twitter":
+          zocialClass = "zocial-twitter";
+          myData = "Twitter";
+          break;
+        case "github":
+          zocialClass = "zocial-github";
+          myData = "Github";
+          break;
+        case "linkedin":
+          zocialClass = "zocial-linkedin";
+          myData = "LinkedIn";
+          break;
+        case "wordpress":
+          zocialClass = "zocial-wordpress";
+          myData = "Wordpress";
+          break;
+        default:
+          zocialClass = "";
+          myData = contactKey;
+      };
+
+      myRef = bio.footercontacts[contactKey];
+
+      // format and display contact
+      var formattedContact = HTMLfooterGeneric.replace("%zocial%", zocialClass).replace("#", myRef).replace("%data%", myData);
+      $("#footerContacts").append(formattedContact);
+    }
+  };
 }
 
 // Main function to display the resume
@@ -343,7 +428,8 @@ function displayResume() {
   $("#mapDiv").append(googleMap);
 
   // call function to display contacts in footer
-  displayContacts("#footerContacts");
+  //displayContacts("#footerContacts");
+  displayFooter();
 }
 
 displayResume();
